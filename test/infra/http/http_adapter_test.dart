4d1f4cbbe0d94d3690b1a57c91522c73f0192801
João Baroni <joao.silva@vrsoft.com.br>
2021-07-20 20:34:03 -0300

    ensure HttpAdapter throws unauthorized on post returns 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -100,6 +100,14 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test('Should return server error if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: Uri.parse(url), method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
     test('Should return server error if post returns 500', () async {
       mockResponse(500);
 
