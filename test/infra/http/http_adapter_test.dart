79eb4c379c1a8398680bf35ac32aeea5d4f28819
João Baroni <joao.silva@vrsoft.com.br>
2021-07-19 21:55:53 -0300

    ensure HttpAdapter calls post with correct body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,5 +1,7 @@
 // @dart=2.10
 
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -11,35 +13,39 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({@required Uri url, @required String method}) async {
+  Future<void> request(
+      {@required Uri url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(url, headers: headers);
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
 class ClientSpy extends Mock implements Client {}
 
 void main() {
-  Uri url;
+  String url;
   HttpAdapter sut;
   ClientSpy client;
 
   setUp(() {
-    final client = ClientSpy();
-    final sut = HttpAdapter(client);
-    final url = Uri.parse(faker.internet.httpUrl());
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
   });
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(
+          url: Uri.parse(url), method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(url, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      }));
+      verify(client.post(Uri.parse(url),
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: '{"any_key":"any_value"}'));
     });
   });
 }
